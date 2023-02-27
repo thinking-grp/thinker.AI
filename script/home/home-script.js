f9a1bb1ac3bb2f048fe41f11dceb21cd29995324
EARTH <hiyudon0524@gmail.com>
2023-02-27 16:56:41 +0900

    s

--- script/home/home-script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener("scroll", function () {
-  const blockthree = document.querySelector(".block-three");
-  blockthree.classList.toggle("topthree0", window.scrollY > 3850);
-});
